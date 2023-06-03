b0b44e4afc409a2abc7963043e2ce0224d354784
Miguel Camilo <miguelcamilo48@gmail.com>
2023-06-03 11:07:34 -0400

    Building out recovery and password component page

--- src/components/Recovery.jsx
+++ src/components/Recovery.jsx
@@ -1,11 +1,63 @@
+import styles from "../styles/Username.module.css";
+import { Toaster } from "react-hot-toast";
 
 const Recovery = () => {
+	return (
+		<div className="container mx-auto">
+			<Toaster
+				position="top-center"
+				reverseOrder={false}
+				toastOptions={{
+					duration: 2000,
+				}}
+			/>
+			<div className="flex justify-center items-center h-screen">
+				<div className={styles.glass}>
+					<div className="title flex flex-col items-center">
+						<h4 className="text-5xl font-bold">Recover Password</h4>
+						<span className="py-4 text-xl w-2/3 text-center text-gray-500">
+							Enter OTP to recover password.
+						</span>
 
-    return (
-        <>
-        Recovery
-        </>
-    )
-}
+						<form className="pt-20">
+							{/* <div className="profile flex justify-center py-4">
+								<img src={avatar} alt="avatar" className={styles.profile_img} />
+							</div> */}
 
-export default Recovery
+							<div className="textbox flex flex-col items-center gap-6">
+								<div className="input text-center">
+									<span className="py-4 text-sm text-left text-gray-500 ">
+										Enter 6 digit OTP sent to your email address.
+									</span>
+									<input
+										type="text"
+										inputmode="numeric"
+										autocomplete="one-time-code"
+										placeholder="OTP"
+										className={styles.textbox}
+									/>
+								</div>
+
+								<button
+									type="submit"
+									className="border bg-indigo-500 w-full py-2 rounded-lg text-gray-50 text-xl shadow-sm text-center hover:bg-[#ff6a6a]"
+								>
+									Recover
+								</button>
+							</div>
+
+							<div className="text-center py-4">
+								<span className="text-gray-500">
+									Didn't get OTP? {""}
+									<button className="text-red-600">Resend</button>
+								</span>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Recovery;
--- src/helper/validate.jsx
+++ src/helper/validate.jsx
@@ -1,5 +1,17 @@
 import toast from 'react-hot-toast'
 
+// validate username
+function usernameVerify(error = {}, values){
+    if(!values.username){
+        error.username = toast.error("Username Required!");
+        
+    }else if(values.username.includes(" ")){
+        error.username = toast.error("Invalid Username!")
+    }
+
+    return error;
+}
+
 // validate login username
 export const usernameValidate = async (values) => {
     const errors = usernameVerify({}, values)
@@ -7,13 +19,30 @@ export const usernameValidate = async (values) => {
     return errors
 }
 
-function usernameVerify(error = {}, values){
-    if(!values.username){
-        error.username = toast.error('Username Required!');
-        
-    }else if(values.username.includes(" ")){
-        error.username = toast.error('Invalid Username!')
+
+// validate password 
+function passwordVerify(errors = {}, values) {
+    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+
+    if(!values.password) {
+        errors.password = toast.error("Password Required!")
+
+    } else if (values.password.includes(" ")) {
+        errors.password = toast.error("Wrong Password!")
+
+    } else if(values.password.length < 4) {
+        errors.password = toast.error("Password must be more than 4 characters!")
+
+    } else if (!specialChars.test(values.password)) {
+        errors.password = toast.error("Password must have special characters!")
     }
 
-    return error;
+    return errors
+}
+
+// validate password
+export const passwordValidate = async (values) => {
+    const errors = passwordVerify({}, values)
+
+    return errors
 }
